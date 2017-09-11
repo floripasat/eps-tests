2d408f961494026a1a6dd8672cc48d6fa6c1c127
Unknown <brunoeiterer@gmail.com>
2017-09-11 09:55:16 -0300

    Add debug led definitions

---
+++ include/hal.h
@@ -0,0 +1,15 @@
+/*
+ * hal.h
+ *
+ *  Created on: 11/09/2017
+ *      Author: Bruno Eiterer
+ */
+
+#ifndef HAL_H_
+#define HAL_H_
+
+#define debugLedDir	P5DIR
+#define debugLedPort P5OUT
+#define debugLedPin BIT4
+
+#endif /* HAL_H_ */
